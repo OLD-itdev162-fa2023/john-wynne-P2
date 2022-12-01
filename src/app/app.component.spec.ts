15d630d836d4d26d8491b5fe5b02d5d350e61321
John Wynne <wynnej1@gamatc.matc.edu>
2022-11-30 22:25:36 -0600

    fixes to p2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-video-game-db'`, () => {
+  it(`should have as title 'john-wynnep2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-video-game-db');
+    expect(app.title).toEqual('john-wynnep2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-video-game-db app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('john-wynnep2 app is running!');
   });
 });
